4bb315b080231f59d822a83a181ffe122dc746f3
Naim Najmi <naimnajmi04@gmail.com>
2023-10-02 22:56:41 +0800

    add supplier file
    add package beans dengan model jugak

---
+++ src/main/java/com/Megathlon/Beans/Suppliers.java
@@ -0,0 +1,32 @@
+package com.Megathlon.Beans;
+
+public class Suppliers {
+	private int supplierID;
+	private String supplierName, supplierContact, supplierLocation;
+	public int getSupplierID() {
+		return supplierID;
+	}
+	public void setSupplierID(int supplierID) {
+		this.supplierID = supplierID;
+	}
+	public String getSupplierName() {
+		return supplierName;
+	}
+	public void setSupplierName(String supplierName) {
+		this.supplierName = supplierName;
+	}
+	public String getSupplierContact() {
+		return supplierContact;
+	}
+	public void setSupplierContact(String supplierContact) {
+		this.supplierContact = supplierContact;
+	}
+	public String getSupplierLocation() {
+		return supplierLocation;
+	}
+	public void setSupplierLocation(String supplierLocation) {
+		this.supplierLocation = supplierLocation;
+	}
+
+}
+
